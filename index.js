8d7d66d6675ad828db6ff80d0e4e4b2f75cede3e
Andrés Felipe Torres Castro <andres.torres1901@alumnos.ubiobio.cl>
2022-11-13 16:10:49 -0300

    Inicializacion proyecto

--- index.js
+++ index.js
@@ -4,4 +4,18 @@ const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
-app.listen(3000, ()=> console.log('Server started'));
+app.listen(process.env.PORT, ()=> {
+        console.log('El proyecto esta corriendo en el puerto ->', process.env.PORT)
+});
+
+mongoose.set('useFindAndModify', false);
+mongoose.set('useNewUrlParser', true);
+mongoose.set('useCreateIndex', true);
+mongoose.set('useUnifiedTopology', true);
+
+mongoose.connect(process.env.DB,(err)=>{
+    if(err){
+        return console.log('Error al conectar con la base de datos ->'. err)
+    }
+    return console.log('Conectado a la base de datos')
+});
